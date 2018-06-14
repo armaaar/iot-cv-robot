617c009b6f0ed536266497bfacba58f04282f423
Ahmed Rafik <ahmedrafik_maaar@outlook.com>
2018-06-14 17:45:15 +0200

    Feat: Add console code and stream check

--- static/scripts.js
+++ static/scripts.js
@@ -1,3 +1,32 @@
+/* Elements selectors */
+selectors = {
+    console: ".console pre",
+    stream: ".stream-container img"
+}
+
+/* Global Functions */
+
+function writeToConsole(textToWrite) {
+    var $console = $(selectors.console),
+        startSymbol = '>';
+    $console.append(startSymbol+" "+textToWrite+"\n");
+    $console.scrollTop($console.prop("scrollHeight"));
+    return true;
+}
+
+function checkStream() {
+    var img = new Image(),
+        streamURL = $(selectors.stream).attr('src');
+    img.onload = function() {
+        writeToConsole('Stream is Ready!');
+    }
+    img.onerror = function(){
+        writeToConsole('Stream has Error!');
+    }
+    img.src = streamURL;
+}
+
 $( document ).ready(function() {
-    
+    // Check if stream kicked off
+    checkStream();
 });
